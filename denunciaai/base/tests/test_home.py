1095bf3a2acfbf775598eea1956e071e2a513fad
Thiago Garcia <thiago-garcia@outlook.com>
2025-04-12 10:42:24 -0300

    Criado template da home e testes

--- denunciaai/base/tests/test_home.py
+++ denunciaai/base/tests/test_home.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 import pytest
 
+from denunciaai.django_assertions import dj_assert_contains
+
 
 @pytest.fixture
 def resp(client):
@@ -10,3 +12,19 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_form_consulta(resp):
+    dj_assert_contains(resp, '<form')
+
+
+def test_form_input_chave(resp):
+    dj_assert_contains(resp, '<input type="text" name="chave"')
+
+
+def test_link_abrir_denuncia(resp):
+    dj_assert_contains(resp, '<a href="/denuncia/abrir"')
+
+
+def test_link_restrito(resp):
+    dj_assert_contains(resp, '<a href="/restrito"')
